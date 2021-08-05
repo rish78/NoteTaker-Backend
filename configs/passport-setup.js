43e8aadd685038935e1ac8c518af9223651a8ad4
Rishabh <rishabhkr.073@gmail.com>
2021-08-06 04:31:41 +0530

    Committing changes

--- configs/passport-setup.js
+++ configs/passport-setup.js
@@ -14,27 +14,21 @@ passport.use(
         clientSecret: process.env.CLIENT_SECRET,
         callbackURL: "http://localhost:8000/auth/google/redirect",
       },
+      
       // function to get profile details and a call back function
       function (accessToken, refreshToken, profile, done) {
+        console.log(`${process.env.CLIENT_ID}`);
         return done(null, profile);
       }
     )
   );
 
   passport.serializeUser(function(user, done) {
-    /*
-    From the user take just the id (to minimize the cookie size) and just pass the id of the user
-    to the done callback
-    PS: You dont have to do it like this its just usually done like this
-    */
+    
     done(null, user);
   });
   
 passport.deserializeUser(function(user, done) {
-    /*
-    Instead of user this function usually recives the id 
-    then you use the id to select the user from the db and pass the user obj to the done callback
-    PS: You can later access this data in any routes in: req.user
-    */
+   
     done(null, user);
 });
